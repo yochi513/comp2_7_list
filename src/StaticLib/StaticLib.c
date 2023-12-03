ec5912858e0b0e252b9e75c688c131ad26427c9e
yochi513 <yojrdkt@gmail.com>
2023-12-03 19:25:21 +0900

    テスト通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -1,40 +1,43 @@
-﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
-#include "Windows.h"                    // Windows API の機能定義
-
+﻿#define WIN32_LEAN_AND_MEAN
+#include "Windows.h"
 #include "../include/lib_func.h"
-
 
 // ノードを初期化
 void initialize_node(node* p, int val)
 {
-	p->pNext = NULL;
-	p->data = val;
+    p->pNext = NULL;
+    p->data = val;
 }
 
 // リストを初期化
 void initialize_list(list* l)
 {
-	l->header = NULL;
+    l->header = NULL;
 }
-
 
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
-	// ToDo: リストにノードを追加するソースコードを実装する
+    // ノードpをリストlの先頭に追加
+    p->pNext = l->header;
+    l->header = p;
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
-	// ToDo: リストからpで指定したノードの次のノードを削除する
-}
+    if (!p || !p->pNext) return; // pがNULLまたはpの次のノードが存在しない場合は何もしない
 
+    node* temp = p->pNext;  
+    p->pNext = temp->pNext; 
+
+    
+}
 
 // pの次のノードを取得
 node* get_next(node* p)
 {
-	if (!p) return NULL;
+    if (!p) return NULL;
 
-	return p->pNext;
+    return p->pNext;
 }
